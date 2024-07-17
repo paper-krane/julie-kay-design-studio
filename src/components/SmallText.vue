57985ea12c065f6c90a84d46d26954f6724ebdf2
Benjamin Farnham <20448279+paper-krane@users.noreply.github.com>
2024-07-16 19:57:32 -0600

    Added button

--- src/components/SmallText.vue
+++ src/components/SmallText.vue
@@ -2,12 +2,15 @@
     <section class="pk__small-text_section pk__section-margin-top pk__section-margin-bot">
         <div class="pk__container pk__m">
             <span class="pk__small-text_percent">15mins</span>
-            <p>That's all it takes to get started. Schedule your 15 minute discovery call and together, we will design and create something truly breathtaking for you and your family.</p>
+            <p>That's all it takes to get started! Schedule your 15 minute discovery call and together, we will begin the process to design and create something truly breathtaking for you and your family.</p>
+            <Button href="#">Schedule Consultation</Button>
         </div>
     </section>
 </template>
 
 <script setup>
+// Components
+import Button from '@/components/Button.vue';
 
 </script>
 
